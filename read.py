e5c7d0988dc61cd3a6bd26207a3d7dc37e962a9c
Jason Chen <purplvampire@gmail.com>
2019-03-23 16:38:33 +0800

    add word filtering

--- read.py
+++ read.py
@@ -6,14 +6,14 @@ with open('reviews.txt', 'r') as f:
         count += 1
         # if count % 1000 == 0:
         #     print(len(data))
-# print('檔案讀取完了, 總共有', len(data), '筆資料!')
+print('檔案讀取完了, 總共有', len(data), '筆資料!')
 
 # 求留言的平均長度
 sum_len = 0
 for d in data:
     sum_len += len(d) # sum_len = sum_len + len(d)
 avg = sum_len / len(data) # 字串的總長/清單的總筆數
-# print('留言的平均長度為', avg, '個字元')
+print('留言的平均長度為', avg, '個字元')
 
 
 # print(data[0])
@@ -26,6 +26,7 @@ for d in data:
         new.append(d)
 print('一共有', len(new), '筆留言長度小於100')
 
+# 隨機撈出其中一筆留言
 import random
 x = random.randint(0, len(new))
 print('以下是第', x, '筆留言')
@@ -36,6 +37,8 @@ for d in data:              # 在data清單中取出每一筆留言
     if 'good' in d:         # 如果d留言中有'good'字串(True)
         good.append(d)      # 將該留言加到good清單中
 print('一共有', len(good), '筆留言提到Good')
+
+# 隨機撈出其中一筆留言
 y = random.randint(0, len(good))
 print('這是第', y, '筆留言')
 print(good[y])
